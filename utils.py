4a2f29ec85b27d9864dd956265dd70ad02db46a1
Ornatelli Antonio (DATA ICT) <antonio.ornatelli@enel.com>
2025-01-31 14:23:38 +0100

    add parameter s with default 1, remove terms for shifted version in mipgap

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def compute_weights(n: int, lb: list, ub: list, epsilon: list[float] | float, k: float) -> tuple:
+def compute_weights(n: int, lb: list, ub: list, epsilon: list[float] | float, k: float, s: float = 1.0) -> tuple:
     """ Algorithm to compute the weights for the single objective problem. """
     if isinstance(epsilon, float):
         epsilon = [epsilon] * n
@@ -10,10 +10,9 @@ def compute_weights(n: int, lb: list, ub: list, epsilon: list[float] | float, k:
     for i in range(1, n):
         lambda_.append(lambda_[i - 1] * epsilon[i - 1] / (ub[i] - lb[i]) / (1 + (epsilon[i] / (ub[i] - lb[i]))))
     delta = k / (lambda_[n - 1] * epsilon[-1])
-    mip_gap = lambda_[n - 1] * epsilon[-1] * (ub[0] - lb[0]) / ub[0]
+    mip_gap = s * lambda_[n - 1] * epsilon[-1]
     weights = [delta*lambda_[i] for i in range(n)]
     return weights, mip_gap
-
 
 def get_random_rotation(n) -> np.ndarray:
     T = np.random.rand(n, n)
